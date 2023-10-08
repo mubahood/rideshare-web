b7ce5795254167ec50f8bce843f3d089dc9ee75d
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-10-08 23:45:37 +0300

    status

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Trip extends Model
 {
     use HasFactory;
+
+    //boot
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            //can't create a trip while having another trip in pending status
+            $pending_trips = Trip::where([
+                'status' => 'Pending',
+                'driver_id' => $model->driver_id
+            ])->first();
+            if ($pending_trips) {
+                throw new \Exception("You can't create a trip while having another trip in pending status.");
+                return false;
+            }
+        });
+    }
 }
