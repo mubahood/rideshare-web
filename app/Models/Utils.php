91c3dff8130d01a96b30ca36188e2fee46eb9988
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-10-08 12:42:12 +0300

    otp

--- app/Models/Utils.php
+++ app/Models/Utils.php
@@ -32,7 +32,7 @@ class Utils extends Model
         $otp = rand(100000, 999999);
         $u->otp = $otp;
         $u->save();
-        $message = "Dear {$u->name}, Your RIDESHARE OTP CODE is {$otp}";
+        $message = "Your RIDESHARE OTP CODE is {$otp}";
         $phone_number = $u->phone_number;
         $response = Utils::send_message($phone_number, $message);
         return $response;
